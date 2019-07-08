72813c452c6bbcd320c9a1fd19e0b9ee3ac0a3dc
Amanda Nascimento <asdn.amanda@gmail.com>
2019-07-08 12:30:09 -0300

    Definição de layout e configuração do Identity

---
+++ ControleEstoque/ControleEstoque/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ControleEstoque.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ControleEstoque.Areas.Identity.IdentityHostingStartup))]
+namespace ControleEstoque.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- ControleEstoque/ControleEstoque/wwwroot/js/site.js
+++ ControleEstoque/ControleEstoque/wwwroot/js/site.js
@@ -1,4 +1,29 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(".cnpj").keydown(function () {
+    try {
+        $(".cnpj").unmask();
+    } catch (e) { }
+
+    //var tamanho = $(".cnpj").val().length;
+
+    //if (tamanho < 11) {
+    //    $(".cnpj").mask("999.999.999-99");
+    //} else {
+        $(".cnpj").mask("99.999.999/9999-99");
+    //}
+
+    // ajustando foco
+    var elem = this;
+    setTimeout(function () {
+        // mudo a posição do seletor
+        elem.selectionStart = elem.selectionEnd = 10000;
+    }, 0);
+    // reaplico o valor para mudar o foco
+    var currentValue = $(this).val();
+    $(this).val('');
+    $(this).val(currentValue);
+});
