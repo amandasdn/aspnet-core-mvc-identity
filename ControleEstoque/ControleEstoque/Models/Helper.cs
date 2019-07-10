38d2dce73e292d644f578666b44e07237ec192e4
Amanda Nascimento <asdn.amanda@gmail.com>
2019-07-10 13:06:07 -0300

    Pequenas otimizações em métodos.

--- ControleEstoque/ControleEstoque/Models/Helper.cs
+++ ControleEstoque/ControleEstoque/Models/Helper.cs
@@ -4,11 +4,16 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace ControleEstoque.Models
 {
     public static class Helper
     {
+        /// <summary>
+        /// Exemplo: nome-imagem.jpeg | Retorno: nome-imagem
+        /// </summary>
+        /// <param name="texto"></param>
         public static string NomeSemExtensao(this string texto)
         {
             int posicao1 = 0;
@@ -16,9 +21,16 @@ namespace ControleEstoque.Models
 
             texto = texto.Substring(posicao1, posicao2);
 
+            texto = texto.ToLower();
+            texto = texto.Trim();
+
             return texto;
         }
 
+        /// <summary>
+        /// Exemplo: nome-imagem.jpeg | Retorno: .jpeg
+        /// </summary>
+        /// <param name="texto"></param>
         public static string SomenteExtensao(this string texto)
         {
             int posicao1 = texto.LastIndexOf('.');
@@ -26,9 +38,17 @@ namespace ControleEstoque.Models
 
             texto = texto.Substring(posicao1, posicao2);
 
+            texto = texto.ToLower();
+            texto = texto.Trim();
+
             return texto;
         }
 
+        /// <summary>
+        /// Remove todos os caracteres especiais, substituindo-os por caracteres simples.
+        /// </summary>
+        /// <param name="texto"></param>
+        /// <returns></returns>
         public static string SimplificarTexto(this string texto)
         {
             StringBuilder sbReturn = new StringBuilder();
@@ -44,6 +64,8 @@ namespace ControleEstoque.Models
             texto = System.Text.RegularExpressions.Regex.Replace(texto, @"[^0-9a-zA-ZéúíóáÉÚÍÓÁèùìòàÈÙÌÒÀõãñÕÃÑêûîôâÊÛÎÔÂëÿüïöäËYÜÏÖÄçÇ]+?", string.Empty);
 
             texto = texto.ToLower().Replace(" ", "");
+
+            texto = texto.Trim();
 
             return texto;
         }
